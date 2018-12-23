730e20be473c2e3b8c7473e2566d22b54668a6ed
Vlad Shmyhlo <aion.stu@gmail.com>
2018-12-23 16:24:11 +0200

    Refactor

---
+++ benchmark.py
@@ -0,0 +1,17 @@
+from tqdm import tqdm
+import torch.utils.data
+from dataset import TrainEvalDataset
+from train import collate_fn
+import os
+
+train_dataset = TrainEvalDataset('./data/LibriSpeech', subset='train-clean-100')
+train_data_loader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=os.cpu_count(),
+    collate_fn=collate_fn,
+    drop_last=True)
+
+for _ in tqdm(train_data_loader):
+    pass
