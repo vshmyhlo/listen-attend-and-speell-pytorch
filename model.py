89ebc52ec85efaf4085223f35b3bf650bf4af3c7
Vlad Shmyhlo <aion.stu@gmail.com>
2018-12-26 16:19:36 +0200

    Refactor

--- model.py
+++ model.py
@@ -220,7 +220,6 @@ class ScaledDotProductAttention(nn.Module):
         return context, weights
 
 
-# TODO: cell type
 class AttentionDecoder(nn.Module):
     def __init__(self, size, vocab_size):
         super().__init__()
@@ -265,6 +264,48 @@ class AttentionDecoder(nn.Module):
         return outputs, weights
 
 
+class DeepAttentionDecoder(nn.Module):
+    def __init__(self, size, vocab_size):
+        super().__init__()
+
+        self.embedding = nn.Embedding(vocab_size, size, padding_idx=0)
+        self.rnn_1 = nn.GRUCell(size * 2, size)
+        self.rnn_2 = nn.GRUCell(size * 2, size)
+        self.attention = DotProductAttention(size)
+        self.output = nn.Linear(size * 2, vocab_size)
+
+    def forward(self, input, features, last_hidden):
+        embeddings = self.embedding(input)
+        # last_hidden = torch.cat([last_hidden[0], last_hidden[1]], -1)
+        # last_hidden = self.project_hidden(last_hidden)
+
+        # TODO: better init
+        context = torch.zeros(embeddings.size(0), embeddings.size(2)).to(embeddings.device)
+        # context = last_hidden.sum(0)
+        # context, _ = self.attention(torch.zeros(input.size(0), self.rnn.hidden_size).to(input.device), features)
+        # context, _ = self.attention(last_hidden, features)
+
+        hidden_1 = None
+        hidden_2 = None
+        outputs = []
+        weights = []
+
+        for t in range(embeddings.size(1)):
+            input = torch.cat([embeddings[:, t, :], context], 1)
+            hidden_1 = self.rnn_1(input, hidden_1)
+            context, weight = self.attention(hidden_1, features)
+            output = torch.cat([hidden_1, context], 1)
+            hidden_2 = self.rnn_2(output, hidden_2)
+            output = self.output(hidden_2)
+            outputs.append(output)
+            weights.append(weight.squeeze(-1))
+
+        outputs = torch.stack(outputs, 1)
+        weights = torch.stack(weights, 1)
+
+        return outputs, weights
+
+
 class Model(nn.Module):
     def __init__(self, size, vocab_size):
         super().__init__()
@@ -272,7 +313,9 @@ class Model(nn.Module):
         self.encoder = PyramidRNNEncoder(size)
         # self.encoder = Conv1dRNNEncoder(size)
         # self.encoder = Conv2dRNNEncoder(size)
-        self.decoder = AttentionDecoder(size, vocab_size)
+
+        # self.decoder = AttentionDecoder(size, vocab_size)
+        self.decoder = DeepAttentionDecoder(size, vocab_size)
 
     def forward(self, spectras, seqs):
         features, last_hidden = self.encoder(spectras)
