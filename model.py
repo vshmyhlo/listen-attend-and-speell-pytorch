5a90762571bde5fb30396c22e7c9a43caa770915
Vlad Shmyhlo <aion.stu@gmail.com>
2018-12-26 14:10:29 +0200

    Refactor

--- model.py
+++ model.py
@@ -28,9 +28,9 @@ class PyramidRNNEncoder(nn.Module):
     def __init__(self, size):
         super().__init__()
 
-        self.rnn_1 = PBRNN(256, size // 4)
-        self.rnn_2 = PBRNN(size, size // 4)
-        self.rnn_3 = PBRNN(size, size // 2)
+        self.rnn_1 = PBRNN(256, size // 2)
+        self.rnn_2 = PBRNN(size * 2, size // 2)
+        self.rnn_3 = PBRNN(size * 2, size // 2)
 
     def forward(self, input):
         input, _ = self.rnn_1(input)
